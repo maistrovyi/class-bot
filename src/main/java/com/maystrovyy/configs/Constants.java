e77aca8b1e9b7658f6f20545cb81b2c564047b2a
Vladimir Maystrovyy <maystrovyy@users.noreply.github.com>
2017-05-22 13:03:31 +0300

    Initial commit

---
+++ src/main/java/com/maystrovyy/configs/Constants.java
@@ -0,0 +1,12 @@
+package com.maystrovyy.configs;
+
+public interface Constants {
+
+    String LOGIN_REGEX = "^[_'.@A-Za-z0-9-]*$";
+
+    String SPRING_PROFILE_DEVELOPMENT = "dev";
+    String SPRING_PROFILE_PRODUCTION = "prod";
+
+    String SYSTEM_ACCOUNT = "system";
+
+}
