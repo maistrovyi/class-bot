c0393fc5655472e6283ed85cdb04841302baca03
Vladimir Maystrovyy <maystrovyy@users.noreply.github.com>
2017-05-27 02:52:17 +0300

    Stable groupName regex checking impl, final tests

--- src/main/java/com/maystrovyy/rozkladj/api/BaseApiOperations.java
+++ src/main/java/com/maystrovyy/rozkladj/api/BaseApiOperations.java
@@ -1,15 +1,19 @@
 package com.maystrovyy.rozkladj.api;
 
-import com.maystrovyy.configs.ApplicationConstants;
-
-import javax.validation.constraints.NotNull;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @FunctionalInterface
 public interface BaseApiOperations<T> {
 
-    T parse(@NotNull String url);
+    Pattern LATINA_LETTER = Pattern.compile("[a-zA-z]");
+    Pattern CYRILLIC_LETTER = Pattern.compile("[а-яА-я]");
+    Pattern DIGIT = Pattern.compile("[0-9]");
+    Pattern MINUS = Pattern.compile("[-]");
+
+    T parse(String url);
 
     default URI getURI(String url) {
         URI uri = null;
@@ -21,14 +25,25 @@ public interface BaseApiOperations<T> {
         return uri;
     }
 
-    default boolean isValidGroupName(@NotNull String groupName) {
-        if (groupName.length() != 5) {
-            groupName = groupName.replaceAll("\\s+","");
+    default boolean matchGroupNameToRegex(String s) {
+        Matcher hasLatinaLetter = LATINA_LETTER.matcher(s);
+        Matcher hasCyrillicLetter = CYRILLIC_LETTER.matcher(s);
+        Matcher hasDigit = DIGIT.matcher(s);
+        Matcher hasMinus = MINUS.matcher(s);
+        return (hasLatinaLetter.find() || hasCyrillicLetter.find()) && hasDigit.find() && hasMinus.find();
+    }
+
+    default boolean isValidGroupName(String groupName) {
+        if (groupName != null) {
             if (groupName.length() != 5) {
-                return false;
+                groupName = groupName.replaceAll("\\s+", "");
+                if (groupName.length() != 5) {
+                    return false;
+                }
             }
+            return matchGroupNameToRegex(groupName);
         }
-        return groupName.matches(ApplicationConstants.GROUP_NAME_REGEXP);
+        return false;
     }
 
 }
