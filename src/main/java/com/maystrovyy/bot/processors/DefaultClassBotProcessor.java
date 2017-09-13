6a28315dff80f32b28869c48cda3f141982aadfc
Volodymyr Maystrovyy <maystrovyy@gmail.com>
2017-09-13 17:06:28 +0300

    Added cool feature for more details info, TODO implement teacher model

--- src/main/java/com/maystrovyy/bot/processors/DefaultClassBotProcessor.java
+++ src/main/java/com/maystrovyy/bot/processors/DefaultClassBotProcessor.java
@@ -12,12 +12,21 @@ import com.maystrovyy.services.UserService;
 import com.maystrovyy.utils.managers.ScheduleManager;
 import com.maystrovyy.utils.managers.UserDtoManager;
 import io.vavr.control.Try;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.api.methods.BotApiMethod;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.updatingmessages.EditMessageCaption;
+import org.telegram.telegrambots.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.updateshandlers.SentCallback;
 
+import java.io.Serializable;
+
+@Slf4j
 @Component
 public final class DefaultClassBotProcessor implements ClassBotProcessor {
 
@@ -79,7 +88,8 @@ public final class DefaultClassBotProcessor implements ClassBotProcessor {
                     String groupName = persistedUser.getGroupName();
                     Schedule schedule = scheduleService.findByGroupName(groupName);
                     if (schedule != null) {
-                        send(createMessage(chatId, scheduleManager.getTodaySchedule(schedule)));
+//                        send(createMessage(chatId, scheduleManager.getTodaySchedule(schedule)));
+                        sendAsync(createMessageForSchedule(chatId, scheduleManager.getTodaySchedule(schedule)));
                     } else {
                         send(createMessageWithKeyboard(chatId, "Бляха, сталась якась помилка...", menuKeyboard()));
                     }
@@ -102,6 +112,9 @@ public final class DefaultClassBotProcessor implements ClassBotProcessor {
                         send(createMessageWithKeyboard(chatId, "Бляха, сталась якась помилка...", menuKeyboard()));
                     }
                     break;
+                case "Допомога":
+//                    send(test(chatId));
+                    break;
                 case "Назад": {
                     send(createMessageWithKeyboard(chatId, "На головну...", menuKeyboard()));
                     break;
@@ -116,22 +129,68 @@ public final class DefaultClassBotProcessor implements ClassBotProcessor {
     public void processCallback(Update update) {
         String data = update.getCallbackQuery().getData();
         User persistedUser = userService.findByUserName(update.getCallbackQuery().getFrom().getUserName());
+        Long chatId = update.getCallbackQuery().getMessage().getChatId();
         switch (data) {
+            case "%details%":
+                editAsync(editMessageForSchedule(update.getCallbackQuery()));
+                break;
             case "%iamstudent%":
                 persistedUser.setRole(User.Role.STUDENT);
                 userService.update(persistedUser);
-                send(createMessage(update.getCallbackQuery().getMessage().getChatId(), "Харош, скажи но свою групу, наприклад  \"ВВ-41\"."));
+                send(createMessage(chatId, "Харош, скажи но свою групу, наприклад  \"ВВ-41\"."));
                 break;
             case "%iamteacher%":
                 persistedUser.setRole(User.Role.TEACHER);
                 userService.update(persistedUser);
-                send(createMessage(update.getCallbackQuery().getMessage().getChatId(), "Вітаю, Вас, шановний!"));
+                send(createMessage(chatId, "Вітаю, Вас, шановний!"));
                 break;
         }
     }
 
     private void send(SendMessage message) {
         Try.of(() -> classBot.execute(message));
+    }
+
+    private void sendAsync(SendMessage message) {
+        Try.run(() -> classBot.executeAsync(message, new SentCallback<Message>() {
+            @Override
+            public void onResult(BotApiMethod<Message> botApiMethod, Message message) {
+
+            }
+
+            @Override
+            public void onError(BotApiMethod<Message> botApiMethod, TelegramApiRequestException e) {
+                log.error(e.getLocalizedMessage());
+            }
+
+            @Override
+            public void onException(BotApiMethod<Message> botApiMethod, Exception e) {
+                log.error(e.getLocalizedMessage());
+            }
+        }));
+    }
+
+    private void edit(EditMessageCaption message) {
+        Try.of(() -> classBot.execute(message));
+    }
+
+    private void editAsync(EditMessageText message) {
+        Try.run(() -> classBot.executeAsync(message, new SentCallback<Serializable>() {
+            @Override
+            public void onResult(BotApiMethod<Serializable> botApiMethod, Serializable serializable) {
+
+            }
+
+            @Override
+            public void onError(BotApiMethod<Serializable> botApiMethod, TelegramApiRequestException e) {
+                log.error(e.getLocalizedMessage());
+            }
+
+            @Override
+            public void onException(BotApiMethod<Serializable> botApiMethod, Exception e) {
+                log.error(e.getLocalizedMessage());
+            }
+        }));
     }
 
 }
